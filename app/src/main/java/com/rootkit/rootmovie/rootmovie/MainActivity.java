0c4264a2d8c786f3f6c2f661d398d644df29124b
rootkit33 <rootkit3333@gmail.com>
2016-11-07 08:39:34 +0700

    RootMovie

---
+++ app/src/main/java/com/rootkit/rootmovie/rootmovie/MainActivity.java
@@ -0,0 +1,13 @@
+package com.rootkit.rootmovie.rootmovie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
